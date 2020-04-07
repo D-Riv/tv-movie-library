d0ac0d8f98ff7ad35b5f2cb6e94a28c0876b043a
Dennis Rivera <rivd19@outlook.com>
2020-04-07 14:22:21 -0400

    Added modal structure by importing bootstrap theme into movie modal

---
+++ src/containers/movies/MovieModal.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+
+const MovieModal = (props) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const myList = () => libraryData.push(props.title);
+  console.log(props);
+  return (
+    <>
+      <div className="buttons">
+        {/* <Button size="sm" className="modalbutton" onClick={handleShow}>
+          Info
+        </Button> */}
+
+        <i
+          className="fas fa-question-circle"
+          onClick={handleShow}
+          style={{
+            fontSize: "30px",
+            cursor: "pointer",
+            color: "red",
+          }}
+        ></i>
+
+        <i
+          className="fas fa-plus-circle"
+          onClick={myList}
+          style={{
+            fontSize: "30px",
+            cursor: "pointer",
+            color: "lightblue",
+          }}
+        ></i>
+      </div>
+
+      <Modal show={show} onHide={handleClose} centered className="modal">
+        <Modal.Header
+          closeButton
+          style={{ backgroundColor: "black", color: "white" }}
+        >
+          <Modal.Title className="modalhead">{props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body style={{ backgroundColor: "black", color: "white" }}>
+          {props.description} <br /> <br />
+          Rating: {props.rating} <br /> <br />
+          Release Date: {props.releaseDate}
+        </Modal.Body>
+        <Modal.Footer style={{ backgroundColor: "black", color: "white" }}>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+  return <div className="modalContainer"></div>;
+};
+
+export default MovieModal;
