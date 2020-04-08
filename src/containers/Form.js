1c11eddd3094cd3e6a45e6e3c2bb66793013f5d1
Dennis Rivera <rivd19@outlook.com>
2020-04-08 10:18:10 -0400

    Added state to the movie form

--- src/containers/Form.js
+++ src/containers/Form.js
@@ -4,10 +4,65 @@ import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: "",
+      description: "",
+      rating: "",
+      releaseDate: "",
+      image: "",
+    };
+  }
+
   render() {
+    const { name, description, rating, releaseDate, image } = this.state;
     return (
       <div>
         <Navbar />
+        <form>
+          <div>
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="description"
+              value={description}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="rating"
+              value={rating}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="releaseDate"
+              value={releaseDate}
+              onChange={this.changeHandler}
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="image"
+              value={image}
+              onChange={this.changeHandler}
+            />
+          </div>
+        </form>
         <Link>
           <button>Return to Movies</button>
         </Link>
