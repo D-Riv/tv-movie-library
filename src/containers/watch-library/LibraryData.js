ae482d3da1816e5810cd61cf839bf7c06eb334cb
Dennis Rivera <rivd19@outlook.com>
2020-04-07 22:04:49 -0400

    Worked through library data error. State is updating in the library component but refreshes when switching between components

--- src/containers/watch-library/LibraryData.js
+++ src/containers/watch-library/LibraryData.js
@@ -6,40 +6,46 @@ class LibraryData extends React.Component {
     super(props);
 
     this.state = {
-      libraryData: [],
+      libraryData: this.props.data,
     };
   }
-  componentDidMount() {
-    this.addMovie();
-  }
-
-  addMovie = () => {
+  // componentDidMount() {
+  //   // this.addMovie();
+  //   this.setState({ libraryData: this.props.data });
+  // }
+  addMovie = (e) => {
+    e.preventDefault();
     this.setState({ libraryData: this.props.data });
-    // this.state.libraryData.push(this.props.data);
+
     console.log(this.state.libraryData);
   };
 
   render() {
-    let movieList = this.state.libraryData.map((item) => {
-      return (
-        <div className="movieList" key={item.id}>
-          <Card style={{ width: "15rem", border: "none" }}>
-            <Card.Img variant="top" src={item.image} />
-            <Card.Body className="cardfooter">
-              <Card.Title></Card.Title>
-              {/* <MovieModal
-                description={item.description}
-                title={item.name}
-                rating={item.rating}
-                releaseDate={item.releaseDate}
-                library={myList}
-              /> */}
-            </Card.Body>
-          </Card>
-        </div>
-      );
-    });
-    return <div>{movieList}</div>;
+    // let movieList = this.state.libraryData.map((item) => {
+    //   return (
+    //     <div className="movieList" key={item.id}>
+    //       <Card style={{ width: "15rem", border: "none" }}>
+    //         <Card.Img variant="top" src={item.image} />
+    //         <Card.Body className="cardfooter">
+    //           <Card.Title></Card.Title>
+    //           {/* <MovieModal
+    //             description={item.description}
+    //             title={item.name}
+    //             rating={item.rating}
+    //             releaseDate={item.releaseDate}
+    //             library={myList}
+    //           /> */}
+    //         </Card.Body>
+    //       </Card>
+    //     </div>
+    //   );
+    // });
+    return (
+      <div>
+        {this.state.libraryData}
+        <button onClick={this.addMovie}></button>
+      </div>
+    );
   }
 }
 
